1863c0c0b317b205e4ef6264eb86a88fe705f28b
LetsGoAway <ethpsa09@gmail.com>
2024-01-31 16:49:22 +0800

    add mouse polling rate calculator

---
+++ polling-rate-calculator/script.js
@@ -0,0 +1,71 @@
+let mouseMovedTimes = 0;
+let hzArray = [];
+let hzMaximum = 0;
+let hzModeMap = new Map();
+let started = false;
+window.addEventListener('mousemove', (ev) => {
+    if (started) {
+        mouseMovedTimes += 1;
+    }
+});
+
+window.setInterval(() => {
+    if (started) {
+        if (mouseMovedTimes != 0) {
+            hzArray.push(mouseMovedTimes);
+        }
+        if (mouseMovedTimes >= hzMaximum) {
+            hzMaximum = mouseMovedTimes;
+            hzModeMap = new Map();
+            document.getElementById("maxHZ").innerText = "Max: " + hzMaximum + "hz";
+            let avg = 0;
+            hzArray.forEach((v) => {
+                avg += v;
+
+                if (hzModeMap.has(v)) {
+                    hzModeMap.set(v, hzModeMap.get(v) + 1);
+                }
+                else {
+                    hzModeMap.set(v, 1);
+                }
+            });
+            avg = avg / hzArray.length;
+            document.getElementById("avgHZ").innerText = "Avg: " + avg + "hz";
+            // Mode Calculation
+            let lastVal = 0;
+            let lastKey = 0;
+            hzModeMap.forEach((val, key) => {
+                if (val > lastVal) {
+                    lastVal = val;
+                    lastKey = key;
+                } ''
+            });
+            document.getElementById("modeHZ").innerText = "Your Mouse's Polling Rate: " + lastKey + "hz";
+        }
+        mouseMovedTimes = 0;
+    }
+}, 1000);
+
+function reset() {
+    hzModeMap = new Map();
+
+    mouseMovedTimes = 0;
+    hzArray = [];
+    hzMaximum = 0;
+    document.getElementById("maxHZ").innerText = "Max: 0hz";
+    document.getElementById("avgHZ").innerText = "Avg: 0hz";
+    document.getElementById("modeHZ").innerText = "Your Mouse's Polling Rate: 0hz";
+
+
+}
+function toggle() {
+    started = !started;
+    if (started) {
+        document.getElementById("toggleStateBut").innerText = "Stop";
+        document.getElementById("notif").innerText = "Move your mouse in a circle on this window to calculate!";
+
+    } else {
+        document.getElementById("toggleStateBut").innerText = "Start";
+        document.getElementById("notif").innerText = "";
+    }
+}
