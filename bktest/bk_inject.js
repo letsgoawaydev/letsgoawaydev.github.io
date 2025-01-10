0dfe06fd27e64091c3934ef9e0da42adb27fd8c7
Ethan <68365423+letsgoawaydev@users.noreply.github.com>
2025-01-10 22:16:46 +0800

    update

--- bktest/bk_inject.js
+++ bktest/bk_inject.js
@@ -1,10 +1,19 @@
-window.addEventListener("touchstart", (ev) => {
-    if (typeof( DeviceMotionEvent.requestPermission ) === "function") {
-        DeviceMotionEvent.requestPermission()
-        .then((response)=>{
-            if (response == 'granted'){
-                window.postMessage("yipeemotion");
-            }
+HTMLCanvasElement.prototype.getContext = ((origFn) => {
+    return function (type, attributes) {
+        if (type == "webgl2") {
+            return null;
+        }
+
+        attributes = Object.assign({}, attributes, {
+            alpha:true,
+            colorSpace: "display-p3",
+            desynchronized: type != "2d",
+            preserveDrawingBuffer: true,
+            antialias: false,
+            powerPreference: "high-performance",
+            preferLowPowerToHighPerformance: false,
+            xrCompatible:true,
         });
-    }
-});
+        return origFn.call(this, type, attributes);
+    };
+})(HTMLCanvasElement.prototype.getContext);
